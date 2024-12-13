d69e7aaa9bc82cd2dbe40b106788aff02f40f7b2
Maaz <shaikhmaaz0005@gmail.com>
2024-12-13 17:22:10 +0530

    changes

--- MasterPage/Signup.aspx.cs
+++ MasterPage/Signup.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,24 @@ namespace MasterPage
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        SqlConnection conn;
         protected void Page_Load(object sender, EventArgs e)
         {
+            string cs = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+            conn = new SqlConnection(cs);
+            conn.Open();
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string user = TextBox1.Text, pass = TextBox3.Text, email = TextBox2.Text, urole = "User";
+
+            string q = $"exec signupproc  '{user}','{email}','{pass}','{urole}'";
+            SqlCommand cmd = new SqlCommand(q, conn);
+            cmd.ExecuteNonQuery();
+            Response.Write("<script>alert('SignUp Sucssefully!!!'); window.location.href='SignIn.aspx';</script>");
         }
     }
 }
